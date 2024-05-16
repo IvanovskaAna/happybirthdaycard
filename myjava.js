b1e70a626e225ee784f7f6b647468e2f19e84ced
IvanovskaAna <146880066+IvanovskaAna@users.noreply.github.com>
2024-05-16 09:39:21 +0200

    Add files via upload

---
+++ myjava.js
@@ -0,0 +1,5 @@
+alert("You said spamming isn't allowed, but you never said anything about websites :)\n\n -Your favorite IT support person, duh");
+const audio = new Audio();
+audio.src = "./sounds/wilma-evil-laugh.mp3";
+const rawr = new Audio();
+rawr.src = "./sounds/rrawr.mp3";
